91f70370cc3726771deba556c324b687a45aec19
Angelakao1994 <angelakao519@gmail.com>
2023-03-29 22:56:39 +0800

    fix index page

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ $('#footer').load('../html/footer.html');
 
 
 
+
 /* "hamburger"-->在每個頁面的連結上加上了一個onclick屬性，當用戶點擊
 這些連結時，會調用goToPage()函數。goToPage()函數的作用是將當前頁面的
 URL設置為我們要跳轉到的URL，這樣就能在當前頁面中跳轉，而不是加載到新頁面。*/
