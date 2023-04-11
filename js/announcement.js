e11c9a66c17c7e4b392d17e0a886801a8cd40b86
Angelakao1994 <angelakao519@gmail.com>
2023-04-11 23:05:27 +0800

    fix footer & copyright mix in the same html, also fix restaurant page

--- js/announcement.js
+++ js/announcement.js
@@ -1,5 +1,4 @@
 $('#header').load('../html/header.html');
 $('#carousel').load('../html/carousel.html');
-$('#copyright').load('../html/copyright.html');
 $('#footer').load('../html/footer.html');
 
